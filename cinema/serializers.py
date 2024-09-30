28152288c8c2d33657d3c896b67723e4a5dd632a
Ihor Utkin <igorutkin2002@gmail.com>
2024-09-30 02:32:28 +0200

    Resolve

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,24 +1,22 @@
 from rest_framework import serializers
+from cinema.models import Actor, Genre, CinemaHall, Movie
 
-from cinema.models import Movie
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ['id', 'first_name', 'last_name']
 
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'name']
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ['id', 'name', 'rows', 'seats_in_row']
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
-        )
-        instance.duration = validated_data.get("duration", instance.duration)
-
-        instance.save()
-
-        return instance
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'description', 'duration']
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,27 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallList,
+    CinemaHallDetail,
+    MovieViewSet
+)
+
+router = routers.DefaultRouter()
+router.register(r"movies", MovieViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+        path("genre/", GenreList.as_view(), name="genre-list"),
+        path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+        path("actor/", ActorList.as_view(), name="actor-list"),
+        path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+        path("cinema_hall/", CinemaHallList.as_view(), name="cinema_hall-list"),
+        path("cinema_hall/<int:pk>/", CinemaHallDetail.as_view(), name="cinema_hall-detail"),
+        path("", include(router.urls)),
 ]
 
 app_name = "cinema"
